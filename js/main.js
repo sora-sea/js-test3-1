38f1ae5a1172fa00a5955f73b3a53dabc78db790
sora-sea <yasu.abc.jda@gmail.com>
2021-05-19 18:20:37 +0900

    課題完了

--- js/main.js
+++ js/main.js
@@ -1,5 +1,34 @@
 'use strict'
 
 {
-  
+  const btn = document.getElementById('btn');
+  let i = 0
+
+  btn.addEventListener('click', () => {
+
+    const elemNewTask = document.getElementById('newtask');
+    const newTask = elemNewTask.value;
+    
+    const taskList = document.getElementById('tasklist');
+
+    const task = document.createElement('tr');
+    taskList.appendChild(task);
+
+    const taskId = document.createElement('td');
+    const taskComment = document.createElement('td');
+    const taskStatus = document.createElement('button');
+    const taskDelete = document.createElement('button');
+
+    taskId.textContent = i++;
+    taskComment.textContent = newTask;
+    taskStatus.textContent = '作業中';
+    taskDelete.textContent = '削除';
+
+    taskList.appendChild(taskId);
+    taskList.appendChild(taskComment);
+    taskList.appendChild(taskStatus);
+    taskList.appendChild(taskDelete);
+
+    elemNewTask.value = '';
+  });
 }
